ef71362cad6bfa69aaacdfcdc3c64557f0408dd5
Coloey <487336863@qq.com>
2022-02-17 17:59:20 +0800

    change router

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,45 @@
-import {createRouter,createWebHistory} from "vue-router"
-import chat from "../components/chat/chat.vue"
-import dialogue from "../components/chat/dialogue.vue"
-import dialogueDetail from "../components/chat/dialogue-detail.vue"
-import contact from "../components/contact/contact.vue"
-import details from "../components/contact/details.vue"
+import {createRouter,createWebHashHistory} from "vue-router"
+//import chat from "../components/chat/chat.vue"
+//import dialogue from "../components/chat/dialogue.vue"
+//import dialogueDetail from "../components/chat/dialogue-detail.vue"
+//import contact from "../components/contact/contact.vue"
+/*import details from "../components/contact/details.vue"
 import contactDialogue from"../components/contact/contact-dialogue.vue"
 import department from "../components/contact/department.vue"
 import self from "../components/self/self.vue"
-import setting from "../components/self/setting.vue"
+import setting from "../components/self/setting.vue"*/
 //import mobileDetails from "../components/contact/mobile-contacts.vue"
 export default createRouter({
-    history:createWebHistory(),
+    history:createWebHashHistory(),
     routes:[{
         path: '/',
         name: "信息",
-        component: chat
+        component: ()=>Promise.resolve(import("../components/chat/chat.vue"))
     }, {
         path: '/chat/dialogue/',
         name: "",
         components: {
-            "default":chat,
-            "subPage":dialogue
+            "default":()=>Promise.resolve(import("../components/chat/chat.vue")),
+            "subPage":()=>Promise.resolve(import("../components/chat/dialogue.vue"))
         }
     },
     {
         path: '/chat/dialogue/dialogue-detail/',
         name: "dialogueDetail",
-        components: {"subPage": dialogueDetail},
+        components: {"subPage":()=> Promise.resolve(import("../components/chat/dialogue-detail.vue"))},
 
     },
     {
         path: '/contact',
         name: "通讯录",
-        component:contact
+        component:()=>Promise.resolve(import("../components/contact/contact.vue"))
     },
     {
         path:'/contact/department',
         name:"",
         components:{
-            'default':contact,
-            "subPage":department
+            'default':()=>Promise.resolve(import("../components/contact/contact.vue")),
+            "subPage":()=>Promise.resolve(import("../components/contact/department.vue"))
         }
         
     },
@@ -47,27 +47,27 @@ export default createRouter({
         path: '/contact/details/',
         name: "",
         components: {
-            "default": contact,
-            "subPage":details
+            "default": ()=>Promise.resolve(import("../components/contact/contact.vue")),
+            "subPage":()=>Promise.resolve(import("../components/contact/details.vue"))
         }
     },
     {
         path:'/contact/details/contact-dialogue/',
         name:'',
-        components:{"subPage":contactDialogue}
+        components:{"subPage":()=>Promise.resolve(import("../components/contact/contact-dialogue.vue"))}
 
     },
     {
         path:'/self',
         name:'',
-        component:self,
+        component:()=>Promise.resolve(import("../components/self/self.vue")),
     },
     {
         path:'/self/setting/',
         name:'',
         components:{
-            'default':self,
-            'subPage':setting
+            'default':()=>Promise.resolve(import("../components/self/self.vue")),
+            'subPage':()=>Promise.resolve(import("../components/self/setting.vue"))
         }
     },
 ]
