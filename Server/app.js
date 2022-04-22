de9e60d04eed039589f846f5df4ae030127cb4bf
Coloey <487336863@qq.com>
2022-04-22 21:21:21 +0800

    feat cors

--- Server/app.js
+++ Server/app.js
@@ -44,7 +44,7 @@ const { on } = require('events');
 const server=createServer(app)
 const io=require('socket.io')(server,{
     cors: {
-        origin: "https://coloey.github.io"
+        origin: "https://coloey.github.io/Vue-chat/"
         //origin:"http://localhost:8081"
       }
 })
